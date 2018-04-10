f34500fdc77978c1531ad3abf259ea875c80c830
胡金涛 <victorhjt@163.com>
2018-04-10 17:44:19 +0800

    过滤器的注入

--- 2-Dalston版教程示例/api-gateway-with-eureka/src/main/java/com/didispace/Application.java
+++ 2-Dalston版教程示例/api-gateway-with-eureka/src/main/java/com/didispace/Application.java
@@ -2,14 +2,18 @@ package com.didispace;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.client.SpringCloudApplication;
-import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
-//@EnableZuulProxy
+@EnableZuulProxy
 @SpringCloudApplication
 public class Application {
 
 	public static void main(String[] args) {
 		new SpringApplicationBuilder(Application.class).web(true).run(args);
+	}
+
+	@Bean
+	public AccessFilter accessFilter() {
+		return new AccessFilter();
 	}
 
 }
